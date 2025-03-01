0e90fd95d26ecb73741a33a1b143fd1f484fdeeb
Reid Herrera <reidherrerabusiness@gmail.com>
2025-03-01 15:03:22 -0600

    understanding how to open a file through the program, read it, create it, and handle it :)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+use std::fs::File;
+use std::process::exit;
+
 fn main() {
-    println!("Some status update");
-    eprintln!("Some error update");
+    let x = match File::open("story.txt") {
+        Ok(file) => file,
+        Err(error) => {
+            eprintln!("You got a fuckin error dog: {error:#?}");
+            exit(1)
+        }
+    };
+
+    println!("{x:#?}");
 }
