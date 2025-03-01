f92946f22af0a9de8a2ed3110c0843a52f4b816b
Reid Herrera <reidherrerabusiness@gmail.com>
2025-03-01 17:20:15 -0600

    understanding how to read file content within our directory.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::stdin;
+use std::io::{stdin, Read};
 use std::process::exit;
 
 fn main() {
@@ -12,7 +12,7 @@ fn main() {
         exit(1)
     }
 
-    let opened_file = match File::open(x.trim()) {
+    let mut opened_file = match File::open(x.trim()) {
         Ok(opened) => opened,
         Err(error) => {
             eprint!("Oopsie Poopsies: {error}");
@@ -20,5 +20,14 @@ fn main() {
         }
     };
 
+    let mut file_contents: String = String::new();
+    let read_operation = opened_file.read_to_string(&mut file_contents);
+
+    if let Err(error) = read_operation {
+        eprint!("You done fucked up cousin on line 27: {error}");
+        exit(1)
+    }
+
     println!("{opened_file:#?}");
+    println!("{file_contents:#?}");
 }
