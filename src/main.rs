7ad7f98cf623c344f398c4e69a6cfccc26388c0d
Reid Herrera <reidherrerabusiness@gmail.com>
2025-03-01 17:47:45 -0600

    understanding how to propogate errors

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,12 @@
-use std::fs::File;
-use std::io::{stdin, Read};
-use std::process::exit;
+mod read_file;
+
+use read_file::*;
 
 fn main() {
-    let mut x: String = String::new();
-    println!("Tell me the damn file you want to see!");
-    let user_requested_file = stdin().read_line(&mut x);
+    let file_result = read_file();
 
-    if let Err(error) = user_requested_file {
-        eprint!("You done fucked up cousin: {error}");
-        exit(1)
-    }
-
-    let mut opened_file = match File::open(x.trim()) {
-        Ok(opened) => opened,
-        Err(error) => {
-            eprint!("Oopsie Poopsies: {error}");
-            exit(1);
-        }
+    match file_result {
+        Ok(file) => println!("{file}"),
+        Err(error) => eprintln!("You done fucked up cousin {error}"),
     };
-
-    let mut file_contents: String = String::new();
-    let read_operation = opened_file.read_to_string(&mut file_contents);
-
-    if let Err(error) = read_operation {
-        eprint!("You done fucked up cousin on line 27: {error}");
-        exit(1)
-    }
-
-    println!("{opened_file:#?}");
-    println!("{file_contents:#?}");
 }
