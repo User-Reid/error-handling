669f672eed58087d468418560120b696e17f9954
Reid Herrera <reidherrerabusiness@gmail.com>
2025-03-01 16:00:55 -0600

    understanding how to ask the user for input and take that input as an argument to locate and open a file and what to do in the event of an error :)

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 use std::fs::File;
+use std::io::stdin;
 use std::process::exit;
 
 fn main() {
-    let x = match File::open("story.txt") {
-        Ok(file) => file,
+    let mut x: String = String::new();
+    println!("Tell me the damn file you want to see!");
+    let user_requested_file = stdin().read_line(&mut x);
+
+    if let Err(error) = user_requested_file {
+        eprint!("You done fucked up cousin: {error}");
+        exit(1)
+    }
+
+    let opened_file = match File::open(x.trim()) {
+        Ok(opened) => opened,
         Err(error) => {
-            eprintln!("You got a fuckin error dog: {error:#?}");
-            exit(1)
+            eprint!("Oopsie Poopsies: {error}");
+            exit(1);
         }
     };
 
-    println!("{x:#?}");
+    println!("{opened_file:#?}");
 }
