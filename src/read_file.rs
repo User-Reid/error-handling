429fa4b292167cdb6176973d0cd61bb76a563951
Reid Herrera <reidherrerabusiness@gmail.com>
2025-03-01 18:14:54 -0600

    understanding the ? mark operator and knowing when to use it during result enum return values.

--- src/read_file.rs
+++ src/read_file.rs
@@ -4,25 +4,10 @@ use std::io::{self, stdin, Read};
 pub fn read_file() -> Result<String, io::Error> {
     let mut x: String = String::new();
     println!("Tell me the damn file you want to see!");
-    let user_requested_file = stdin().read_line(&mut x);
-
-    if let Err(error) = user_requested_file {
-        return Err(error);
-    }
-
-    let mut opened_file = match File::open(x.trim()) {
-        Ok(opened) => opened,
-        Err(error) => {
-            return Err(error);
-        }
-    };
+    stdin().read_line(&mut x)?;
 
     let mut file_contents: String = String::new();
-    let read_operation = opened_file.read_to_string(&mut file_contents);
-
-    if let Err(error) = read_operation {
-        return Err(error);
-    }
+    File::open(x.trim())?.read_to_string(&mut file_contents)?;
 
     Ok(file_contents)
 }
