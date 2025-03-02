33eca4c90e2bffe2a0af568d0d233d33b9c7b303
Reid Herrera <reidherrerabusiness@gmail.com>
2025-03-01 18:23:44 -0600

    understanding the read_to_string function within the fs submodule and how it can simplify code :)

--- src/read_file.rs
+++ src/read_file.rs
@@ -1,13 +1,10 @@
-use std::fs::File;
-use std::io::{self, stdin, Read};
+use std::fs;
+use std::io::{self, stdin};
 
 pub fn read_file() -> Result<String, io::Error> {
     let mut x: String = String::new();
     println!("Tell me the damn file you want to see!");
     stdin().read_line(&mut x)?;
 
-    let mut file_contents: String = String::new();
-    File::open(x.trim())?.read_to_string(&mut file_contents)?;
-
-    Ok(file_contents)
+    fs::read_to_string(x.trim())
 }
